be5e387e94ec18b6690384bb5e428f6039734af8
beez0mbie <vzhik3268@gmail.com>
2023-09-20 16:43:10 +0300

    Этап 2: Бекенд - Разное

--- env.js
+++ env.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const {
   PORT = 3000,
   MONGODB_URL = 'mongodb://localhost:27017/bitfilmsdb',
-  NODE_ENV = 'production',
+  NODE_ENV = 'default_node_env',
   JWT_SECRET = 'default_production_secret',
   JWT_DEV = 'default_dev_secret',
 } = process.env;
